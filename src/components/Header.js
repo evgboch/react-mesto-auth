cae52eec038e73556f74f341f544d090fb01a9f3
Evgeny Bocharov <evgbocharov@gmail.com>
2022-08-28 13:18:00 +0300

    feat: настроить выход из приложения

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,14 @@
 import { Link, Route, Switch } from "react-router-dom";
 
-function Header({onLogout}) {
+function Header({onSignOut}) {
     return(
       <header className="header page__header">
         <div className="header__logo"></div>
         <div className="header__button-container">
-          {/* <p className={loggedIn ? "header__email" : "header__email header__email_hidden"}>email@email.com</p>
-          <Link
-            to={!registered ? "/sign-up" : "sign-in"}
-            className={!loggedIn ? "header__button" : "header__button header__button_logged"}
-            onMouseDown={onButtonClick}
-          >
-            {registered ? "Регистрация" : "Войти"}
-          </Link> */}
-
           <Switch>
             <Route exact path="/">
               <p className="header__email">email@email.com</p>
-              <Link to="/signin" className="header__button" onMouseDown={onLogout}>
+              <Link to="/signin" className="header__button" onMouseDown={onSignOut}>
                 Выйти
               </Link>
             </Route>
